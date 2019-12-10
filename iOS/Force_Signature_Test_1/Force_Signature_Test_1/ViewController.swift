e68522b16a9a6b8765189b855dbaa64f393fa19a
OwnMacBookPro <vkqxkr@gmail.com>
2019-12-10 16:08:09 +0900

    수정 성공

--- iOS/Force_Signature_Test_1/Force_Signature_Test_1/ViewController.swift
+++ iOS/Force_Signature_Test_1/Force_Signature_Test_1/ViewController.swift
@@ -14,26 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var GetPicture_Button: UIButton!
     @IBOutlet weak var Reset_Button: UIButton!
     @IBOutlet weak var Save_Button: UIButton!
-    @IBOutlet weak var DrawingSignature_ContainerView: UIView!
+    @IBOutlet weak var DrawingSIgnatureView_ImageView: UIImageView!
     
     var SubjectName_String: String = ""
-    var ViewContoller2_UIViewContoller = ViewController2()
+    
+    var ForceValue_String: String = ""
+    
+    var BrushColor_UIColor = UIColor.black.cgColor
+    var BrushWidth_CGFloat: CGFloat = 1.0
+
+    var LastPoint_CGPoint = CGPoint.zero
+    var IsDrawing_Boolean = false
+    
+    var IsClear_Boolean = false
     
     let Picker_PickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let addViewController = storyboard.instantiateViewController(withIdentifier: "ViewController2_ID")
-//        self.addChild(addViewController)
-//        self.view.addSubview(addViewController.view)
-//        addViewController.didMove(toParent: self)
-        
+        DrawingSIgnatureView_ImageView.layer.borderColor = UIColor.black.cgColor
+        DrawingSIgnatureView_ImageView.layer.borderWidth = 1.0
         Picker_PickerController.delegate = self as  UIImagePickerControllerDelegate & UINavigationControllerDelegate
-        DrawingSignature_ContainerView.frame.size.height = 300
-        DrawingSignature_ContainerView.frame.size.width = 300
-        DrawingSignature_ContainerView.layer.borderWidth = 1.0
         SubjectName_TextField.returnKeyType = .done
         self.SubjectName_TextField.delegate = self as? UITextFieldDelegate
     }
@@ -68,7 +69,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func Reset_Func_Reset_Button(_ sender: UIButton) {
-        ViewContoller2_UIViewContoller.ClearAll_Func()
+        ForceValue_String = ""
+        DrawingSIgnatureView_ImageView.image = nil
     }
     
     @IBAction func Save_Func_Save_Button(_ sender: UIButton) {
@@ -83,6 +85,58 @@ class ViewController: UIViewController {
     func OpenGallary_Func(){
       Picker_PickerController.sourceType = .photoLibrary
       present(Picker_PickerController, animated: false, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+                LastPoint_CGPoint = touch.location(in: DrawingSIgnatureView_ImageView)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let force = touch.force/touch.maximumPossibleForce
+            let currentPoint = touch.location(in: DrawingSIgnatureView_ImageView)
+            let x = currentPoint.x
+            let y = currentPoint.y
+            if x >= 0 && x <= 300 && y >= 0 && y <= 300 {
+                print(force)
+            }
+            DrawLine_Func(from: LastPoint_CGPoint, to: currentPoint, force: force)
+            LastPoint_CGPoint = currentPoint
+
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let force = touch.force/touch.maximumPossibleForce
+            if !IsDrawing_Boolean {
+                DrawLine_Func(from: LastPoint_CGPoint, to: LastPoint_CGPoint, force: force)
+            }
+            IsDrawing_Boolean = false
+        }
+        print(ForceValue_String)
+    }
+    
+    func DrawLine_Func(from: CGPoint, to: CGPoint, force: CGFloat) {
+        UIGraphicsBeginImageContext(DrawingSIgnatureView_ImageView.frame.size)
+
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+
+        DrawingSIgnatureView_ImageView.image?.draw(in: DrawingSIgnatureView_ImageView.bounds)
+
+        context.setLineCap(.round)
+        context.setLineWidth(force * 10)
+        context.setStrokeColor(BrushColor_UIColor)
+        context.move(to: from)
+        context.addLine(to: to)
+
+        context.strokePath()
+
+        DrawingSIgnatureView_ImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+
+        UIGraphicsEndImageContext()
     }
 }
 
